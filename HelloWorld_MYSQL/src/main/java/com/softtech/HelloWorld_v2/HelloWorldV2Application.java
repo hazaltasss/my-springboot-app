e31b4c23c91c3cc5a6bef937798b615325daa3ff
hazaltasss <68058031+hazaltasss@users.noreply.github.com>
2024-09-21 14:15:05 +0300

    Add files via upload

---
+++ HelloWorld_MYSQL/src/main/java/com/softtech/HelloWorld_v2/HelloWorldV2Application.java
@@ -0,0 +1,13 @@
+package com.softtech.HelloWorld_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldV2Application.class, args);
+	}
+
+}
